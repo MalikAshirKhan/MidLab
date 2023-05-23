886a418c9426bfff45ab7b3ba7084a00ba69e3e4
Najeeb Said <tcanjb@gmail.com>
2023-05-23 17:48:25 +0500

    Navigation Done header done pages left

--- Screens/All.js
+++ Screens/All.js
@@ -1,18 +1,28 @@
 import {Button, FlatList, Text, View} from "react-native";
 
 function renderItem({item}) {
-  return <Text style={{fontSize: 30, fontWeight: 'bold',}}>{item}</Text>;
+  return (<View style={{flexDirection: "row"}}>
+        <Text style={{fontSize: 20, fontWeight: 'bold', margin: 10}}>{item.name}</Text>
+        <Text style={{fontSize: 20, fontWeight: 'bold', margin: 10}}>{item.reg}</Text>
+        <Text style={{fontSize: 20, fontWeight: 'bold', margin: 10}}>{item.claass}</Text>
+      </View>)
 }
 
-export default function All({navigation,route}) {
-  const data=route.params.data;
-  return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <FlatList data={data} renderItem={renderItem}/>
-        <Button
-            title="Go Back"
-            onPress={() => navigation.goBack()}
-        />
-      </View>
-  );
+export default function All({navigation, route}) {
+  const data = route.params.data;
+  return (<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View>
+          <FlatList data={data} renderItem={renderItem} style={{
+            width: "100%",
+          }}/>
+        </View>
+        <View style={{
+          position: 'absolute', bottom: 0, left: 0, right: 0, backgroundColor: 'white',
+        }}>
+          <Button
+              title="Go Back"
+              onPress={() => navigation.goBack()}
+          />
+        </View>
+      </View>);
 }
